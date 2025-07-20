6a741a5f5fa89c7d2b75f5390a161f13f9a018f0
Captain-Duckie <austin.fortiner@gmail.com>
2025-07-19 21:32:07 -0400

    Update page.tsx

--- app/experience/[experienceId]/page.tsx
+++ app/experience/[experienceId]/page.tsx
@@ -230,7 +230,9 @@ export default function Landing() {
                 </div>
             </div>
             <Link href="./dashboard">
-                <button className="mt-8 px-6 py-3 bg-blue-600 rounded text-white font-semibold hover:bg-blue-700 transition">Go to Dashboard</button>
+              <a className="mt-8 px-6 py-3 bg-blue-600 rounded text-white font-semibold hover:bg-blue-700 transition">
+                Go to Dashboard
+              </a>
             </Link>
         </div>
     );
