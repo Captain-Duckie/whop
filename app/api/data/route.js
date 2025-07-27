eb2365b4900eb1e5510038a318af9216b72eaad3
Captain-Duckie <austin.fortiner@gmail.com>
2025-07-27 17:58:56 -0400

    Update route.js

--- app/api/data/route.js
+++ app/api/data/route.js
@@ -69,7 +69,7 @@ export async function GET() {
         }), {
             headers: { 
                 'Content-Type': 'application/json',
-                'Cache-Control': 'public, max-age=600, s-maxage=600' // Cache for 50 minutes
+                'Cache-Control': 'public, max-age=20, s-maxage=20' // Cache for 50 minutes
             }
         });
 
